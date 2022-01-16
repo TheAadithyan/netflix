9a75d7f23799ab6b9be24cb5eb4b2d0a060b97e9
Aadithyan Rajesh <hello@trinetsolutions.online>
2022-01-17 00:42:39 +0400

    :memo: Updated Modules

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,16 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import axios from '../../axios';
+import Youtube from 'react-youtube'
 import {API_KEY, imageUrl} from '../Constants/Constants';
 import './RowPost.css';
 
 function RowPost(props) {
   const [Movie, setMovie] = useState([])
   useEffect(() => {
-    axios.get(`discover/movie?api_key=${API_KEY}&with_genres=28`).then((response) => {
+    axios.get(props.url).then((response) => {
       console.log(response);
       setMovie(response.data.results);
     })
   }, [])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
 
   return (
     <div className="row" >
@@ -20,6 +31,7 @@ function RowPost(props) {
                 <img className={props.isSmall ? "smallPoster" : 'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
               ) }
             </div>
+            <Youtube videoId="2g811Eo7K8U" opts={opts} />
     </div>
   )
 }
---
+++ src/urls.js
@@ -0,0 +1,6 @@
+import {API_KEY} from '../src/Components/Constants/Constants'
+
+export const action = `discover/movie?api_key=${API_KEY}&with_genres=28` 
+
+export const originals =`discover/tv?api_key=${API_KEY}&with_networks=213`
+
